cb7d831709e68188b42543e88b26cc377c3ef882
Anas Ali <anasalimgmt@gmail.com>
2024-03-18 22:47:51 +0500

    js-arrays-practice

---
+++ app.js
@@ -0,0 +1,58 @@
+//      CHAPTER 12 TO 16
+//     --- Question 1 ---
+
+// var futureStudents = [];
+// document.write(`Future Students: ${futureStudents}`);
+
+
+//     --- Question 2 ---
+
+// var futureStudents = [];
+// futureStudents.push("Anas");
+// futureStudents.push("Areeb");
+// futureStudents.push("Shayan");
+// document.write(futureStudents);
+
+//     --- Question 3 ---
+
+// var stringsArray = ["Anas", "Areeb" , "Shayan"]
+// document.write(`Strings Array: ${stringsArray}`)
+
+//     --- Question 4 ---
+
+// var numbersArray = [1,2,3,4]
+// document.write(`Numbers Arrays: ${numbersArray}`)
+
+//     --- Question 5 ---
+
+// var booleanArrays = [true, false]
+// document.write(`Boolean Arrays: ${booleanArrays}`)
+
+
+//     --- Question 6 ---
+
+// var mixedArrays = ["Anas", 20 , "SMIT", true , false , 3.14 ]
+// document.write(`<h1>Mixed Arrays: ${mixedArrays}</h1> <br> My Name is ${mixedArrays[0]} <br> I'm ${mixedArrays[1]} Years Old <br> JS Boolean values are ${mixedArrays[3]} & ${mixedArrays[4]} <br> Numeric Value is ${mixedArrays[5]}` )
+
+//     --- Question 7 ---
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil" , "PhD"]
+// document.write(`<h1>QUALIFICATIONS:</h1> 1) ${qualifications[0]} <br>2) ${qualifications[1]} <br>3) ${qualifications[2]} <br>4) ${qualifications[3]} <br> 5) ${qualifications[4]} <br> 6) ${qualifications[5]} <br>7) ${qualifications[6]} <br>8) ${qualifications[7]} <br>  `)
+
+
+//     --- Question 8 ---
+
+// var studentNames = ["Anas", "Areeb", "Shayan"]
+// var studentMarks = [320, 230, 480]
+// var totalMarks = 500;
+
+// var anasTotal = studentMarks[0]/totalMarks*100
+// var areebTotal = studentMarks[1]/totalMarks*100
+// var shayanTotal = studentMarks[2]/totalMarks*100
+
+
+// document.write(`Score of Anas is ${studentMarks[0]}. Percentage: ${anasTotal}% <br> Score of Areeb is ${studentMarks[1]}. Percentage: ${areebTotal}% <br> Score of Shayan is ${studentMarks[2]}. Percentage: ${anasTotal}%`)
+
+//     --- Question 9---
+
+var colors = ["Red","Green"]
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+
+Javascript Assignment 2 - Arrays Practice in this Assignment (Link) - Chapter 12 to 14
